9a24f72966459d3aa5337923c33a4e35085a91d3
Your NameHashimJafarov <hashim.cfr@gmail.com>
2023-09-28 19:52:23 +0400

    Done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const countdownElement = document.getElementById("countdown");
+const countdownElement = document.querySelector(".countdown");
 const startBtn = document.getElementById("start");
 const resetBtn = document.getElementById("reset");
-
+const pauseBtn = document.getElementById("pause");
 let hours = 6;
 let minutes = 0;
 let seconds = 0;
@@ -11,31 +11,60 @@ let intervalId = null;
 const createCountdown = () => {
   if (totalSeconds <= 0) {
     clearInterval(intervalId);
-    countdownElement.innerHTML = "Countdown expired!";
+    startBtn.disabled = true;
+    pauseBtn.disabled = true;
+    resetBtn.disabled = true;
+    countdownElement.innerHTML = `
+    <div>
+    <p class="expired">Countdown expired!</p>
+    </div>
+    `;
   } else {
-    let hours = Math.floor(totalSeconds / 3600);
-    let minutes = Math.floor((totalSeconds % 3600) / 60);
-    let seconds = totalSeconds % 60;
+    hours = Math.floor(totalSeconds / 3600);
+    minutes = Math.floor((totalSeconds % 3600) / 60);
+    seconds = totalSeconds % 60;
     let shownHours = hours < 10 ? "0" + hours : hours;
     let shownMinutes = minutes < 10 ? "0" + minutes : minutes;
     let shownSeconds = seconds < 10 ? "0" + seconds : seconds;
 
     countdownElement.innerHTML = `
-    
-    ${shownHours}h ${shownMinutes}m ${shownSeconds}s`;
+    <div class="countdown_wrapper">
+      <div class="countdown_hours">
+        <p class="timer">${shownHours}</p>
+        <p>Hours</p>
+      </div>
+      <div class="countdown_minutes">
+        <p class="timer">${shownMinutes}</p>
+        <p>Minutes</p>
+      </div>
+      <div class="countdown_seconds">
+        <p class="timer">${shownSeconds}</p>
+        <p>Seconds</p>
+        </div>
+        </div>
+        `;
     totalSeconds--;
   }
 };
-
 startBtn.addEventListener("click", () => {
   intervalId = setInterval(createCountdown, 1000);
   startBtn.disabled = true;
+  pauseBtn.disabled = false;
+  resetBtn.disabled = false;
 });
 
 resetBtn.addEventListener("click", () => {
-  clearInterval(intervalId);
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
   totalSeconds = hours * 3600 + minutes * 60 + seconds;
   createCountdown();
+  startBtn.disabled = true;
+});
+
+pauseBtn.addEventListener("click", () => {
+  pauseBtn.disabled = true;
+  clearInterval(intervalId);
   startBtn.disabled = false;
 });
 
